449f6c096512fb37357f1a5492da522caa0dafa2
SujayGd <sujaygd14@gmail.com>
2020-03-23 19:10:40 -0700

    added checks to path in HDFS in the sripts

--- src/main/bin/load_data.sh
+++ src/main/bin/load_data.sh
@@ -1,105 +1,123 @@
 #!/bin/bash
 
 # Find the script file home
-pushd . > /dev/null
-SCRIPT_DIRECTORY="${BASH_SOURCE[0]}";
-while [ -h "${SCRIPT_DIRECTORY}" ];
-do
-  cd "$(dirname "${SCRIPT_DIRECTORY}")" || exit
-  SCRIPT_DIRECTORY="$(readlink "$(basename "${SCRIPT_DIRECTORY}")")";
-done
-cd "$(dirname "${SCRIPT_DIRECTORY}")" > /dev/null || exit
-SCRIPT_DIRECTORY="$(pwd)";
-popd  > /dev/null || exit
-APP_HOME="$(dirname "${SCRIPT_DIRECTORY}")"
+find_script_home() {
+  pushd . > /dev/null
+  SCRIPT_DIRECTORY="${BASH_SOURCE[0]}";
+  while [ -h "${SCRIPT_DIRECTORY}" ];
+  do
+    cd "$(dirname "${SCRIPT_DIRECTORY}")" || exit
+    SCRIPT_DIRECTORY="$(readlink "$(basename "${SCRIPT_DIRECTORY}")")";
+  done
+  cd "$(dirname "${SCRIPT_DIRECTORY}")" > /dev/null || exit
+  SCRIPT_DIRECTORY="$(pwd)";
+  popd  > /dev/null || exit
+  APP_HOME="$(dirname "${SCRIPT_DIRECTORY}")"
+}
 
+input_checks() {
+  # help for usage of the script
+  if [ "$1" == "-h" ]; 
+  then
+    echo "Usage: $(basename $0) {r1} {c1} {r2} {c2} {BASE_PATH}"
+    exit 0
+  fi
 
-# You can use the $APP_HOME as the absolute path to your project dir
-# This makes navigating your project files easier.
-# This script is run after running scp -i {YOUR_KEY} {PATH/TO/JAR_FILE} {USER}@18.191.83.127
-# Follow the next steps for running an experiment on the cluster
-# 1) Run Python3 {APP_HOME}/python/random_generator.py {r1} {c1} {r2} {c2} to create the left_matrix.txt and right_matrix.txt
-# 2) Remove existing files in the HDFS using hdfs dfs -rm {PATH/TO/FILE}
-# 3) Place the txt files in the HDFS using -
-#     hdfs dfs -put $APP_HOME/bin/left_matrix.txt $BASE_PATH/raw/left/ and 
-#     hdfs dfs -put $APP_HOME/bin/right_matrix.txt $BASE_PATH/raw/right/
-# 4) Delete the left and right matrix txt files from the disk - 
-#     rm $APP_HOME/bin/left_matrix.txt
-#     rm $APP_HOME/bin/right_matrix.txt
-# 5) Run the command - 
-#   nohup spark-submit \
-#   --class edu.asu.sparkpartitioning.TextToObjectFiles \
-#   --master spark://172.31.19.91:7077 \
-#   --deploy-mode client \
-#   ${PATH_TO_JAR}/Spark-Partitioning-0.1-SNAPSHOT.jar \
-#   hdfs://172.31.19.91:9000/{BASE_PATH} hdfs://172.31.19.91:9000/{HISTORY_LOG_DIR} > parsing_logs.log &
+  # checking if the number of args to the script are proper
+  if [ $# -lt 5 ]
+  then
+    echo "Missing Operand"
+    echo "Run $(basename $0) -h for usage"
+    exit 0
+  fi
 
+  echo "Your Input - " 
+  echo "r1 - $1" 
+  echo "c1 - $2" 
+  echo "r2 - $3" 
+  echo "c2 - $4" 
+  echo "Base Path - $5"
+  
+  # checking if c1 == r2 which is required for matrix multiplication
+  if [ $2 != $3 ]
+  then 
+    echo "Error: c1 and r2 should be equal for matrix multiplication"
+    echo "Run `basename $0` -h for usage"
+    exit 0
+  fi
+}
 
+check_HDFS_directories() {
+  
+  BASE_PATH=$1
+  
+  # check if raw folder exists
+  if [[ $(hdfs dfs -test -d $BASE_PATH/raw) = 0 ]]
+  then
+    echo "HDFS directores not found. Creating raw, raw/left and raw/right"
+    hdfs dfs -mkdir -p $BASE_PATH/raw
+    hdfs dfs -mkdir -p $BASE_PATH/raw/left
+    hdfs dfs -mkdir -p $BASE_PATH/raw/right
+  else
+    #check if raw is empty
+    isRawEmpty=$(hdfs dfs -count $BASE_PATH/raw | awk '{print $2}')
+    if [[ $isRawEmpty -eq 0 ]]
+    then
+      #create raw/left and raw/right
+      hdfs dfs -mkdir -p $BASE_PATH/raw/left
+      hdfs dfs -mkdir -p $BASE_PATH/raw/right
+    else
+      #check if raw/left and raw/right are empty or not
+      isEmptyLeft=$(hdfs dfs -count $BASE_PATH/raw/left | awk '{print $2}')
+      isEmptyRight=$(hdfs dfs -count $BASE_PATH/raw/right | awk '{print $2}')
+      if [[ $isEmptyLeft -eq 0 ]] && [[ $isEmptyRight -eq 0 ]]
+      then
+        true
+      else
+        hdfs dfs -rm -r $BASE_PATH/raw/left/*
+        hdfs dfs -rm -r $BASE_PATH/raw/right/*
+      fi
+    fi
+  fi
 
-#   This will convert the txt files to objectfiles
-###############################################################################################################
-# 6)Run the command - 
-#   nohup spark-submit \
-#   --class edu.asu.sparkpartitioning.Main \
-#   --master spark://172.31.19.91:7077 \
-#   --deploy-mode client \
-#   ${PATH_TO_JAR}/Spark-Partitioning-0.1-SNAPSHOT.jar \
-#   hdfs://172.31.19.91:9000/{BASE_PATH} hdfs://172.31.19.91:9000/{HISTORY_LOG_DIR} ${NUM_PARTITION} ${EXPERIMENT} > job_logs_${NUM_PARTITION}.log &
+  # # check if common folder is present in the HDFS
+  # if [[ $(hdfs dfs -test -d $BASE_PATH/common) = 0 ]]
+  # then
+  #   return
+  # else
+  #   hdfs dfs -rm -r $BASE_PATH/common/*
+  # fi
 
-#   This will execute the experiment based on ${EXPERIMENT}
-# 7) Delete everything in the {BASE_PATH}/experiment in the hdfs using 
-#     hdfs dfs -rm -r {BASE_PATH}/{EXPERIMENT}/*
+}
 
-if [ "$1" == "-h" ]; then
-  echo "Usage: `basename $0` {r1} {c1} {r2} {c2} {BASE_PATH}"
-  exit 0
-fi
+main() {
+  find_script_home
+  input_checks $@
+  BASE_PATH=$5
+  PWD=$(pwd)
 
-if [ $# -lt 5 ]
-then
-  echo "Missing Operand"
-  echo "Run `basename $0` -h for usage"
-  exit 0
-fi
+  #check if all the directories required are present in the HDFS
+  check_HDFS_directories $BASE_PATH
 
-printf "Your Input - \n"
-printf "\tr1 - $1\n"
-printf "\tc1 - $2\n"
-printf "\tr2 - $3\n"
-printf "\tc2 - $4\n" 
-printf "\tBase Path - $5\n"
+  # creating the matrices with the python file
+  python3 $APP_HOME/python/random_generator.py $1 $2 $3 $4
 
-if [ $2 != $3 ]
-then 
-  echo "Error: c1 and r2 should be equal for matrix multiplication"
-  echo "Run `basename $0` -h for usage"
-  exit 0
-fi
+  # loading the left and right matrices into the HDFS
+  hdfs dfs -put -f $PWD/left_matrix.txt $BASE_PATH/raw/left/
+  hdfs dfs -put -f $PWD/right_matrix.txt $BASE_PATH/raw/right/
 
-BASE_PATH=$5
-PWD=$(pwd)
+  # delete the left and right matrices from the disk
+  rm $PWD/left_matrix.txt
+  rm $PWD/right_matrix.txt
 
-hdfs dfs -rm -r $BASE_PATH/raw/left/*
-hdfs dfs -rm -r $BASE_PATH/raw/right/*
-hdfs dfs -rm -r $BASE_PATH/common/*
+  # running the spark command to convert txt files to objectfiles
+  spark-submit \
+    --class edu.asu.sparkpartitioning.TextToObjectFiles \
+    --master spark://172.31.19.91:7077 \
+    --deploy-mode client \
+    $APP_HOME/lib/Spark-Partitioning-0.1-SNAPSHOT.jar \
+    hdfs://172.31.19.91:9000$BASE_PATH hdfs://172.31.19.91:9000/spark/applicationHistory > parsing_logs.log 
+}
 
-# creating the matrices with the python file
-python3 $APP_HOME/python/random_generator.py $1 $2 $3 $4
-
-# loading the left and right matrices into the HDFS
-hdfs dfs -put -f $PWD/left_matrix.txt $BASE_PATH/raw/left/
-hdfs dfs -put -f $PWD/right_matrix.txt $BASE_PATH/raw/right/
-
-# delete the left and right matrices from the disk
-rm $PWD/left_matrix.txt
-rm $PWD/right_matrix.txt
-
-# running the spark command to convert txt files to objectfiles
-spark-submit \
-  --class edu.asu.sparkpartitioning.TextToObjectFiles \
-  --master spark://172.31.19.91:7077 \
-  --deploy-mode client \
-  $APP_HOME/lib/Spark-Partitioning-0.1-SNAPSHOT.jar \
-  hdfs://172.31.19.91:9000$BASE_PATH hdfs://172.31.19.91:9000/spark/applicationHistory > parsing_logs.log 
-
+main $@
 

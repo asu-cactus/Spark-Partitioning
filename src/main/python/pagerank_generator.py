87b93677275f4d7f96b35c355c9da5ae61e8fa26
PratikBarhate <pratikbarhate00@outlook.com>
2020-04-04 18:11:14 -0700

    Page rank data generator refactored to match Python3 style from Python2

--- src/main/python/pagerank_generator.py
+++ src/main/python/pagerank_generator.py
@@ -1,39 +1,37 @@
 import random
-import sys
-import time
 import argparse
 
-
 parser = argparse.ArgumentParser(description='generate pagerank data')
-parser.add_argument('numPages',  metavar='numPages', type=int, default=1000000, help='number of pages')
-parser.add_argument('maxLinks', metavar='maxLinks', type=int, default=1000, help='maximum number of links')
-parser.add_argument('fileName', metavar='fileName', type=str, default='data.csv', help='name of output file')
+parser.add_argument('numPages', metavar='numPages', type=int, default=1000000,
+                    help='number of pages')
+parser.add_argument('maxLinks', metavar='maxLinks', type=int, default=1000,
+                    help='maximum number of links')
+parser.add_argument('fileName', metavar='fileName', type=str,
+                    default='data.csv', help='name of output file')
 args = parser.parse_args()
 
-#num of pages
+# num of pages
 numPages = args.numPages
-#maximum num of links
+# maximum num of links
 maxLinks = args.maxLinks
 fileName = args.fileName
 
+assert numPages > maxLinks > 0
 
-assert numPages >  maxLinks and maxLinks > 0
-
-print(str(numPages)+" with max "+ str(maxLinks)+"\n")
+print(str(numPages) + " with max " + str(maxLinks) + "\n")
 
 pages = open(fileName, "w")
 
-for i in xrange(numPages):
+for i in range(numPages):
     numLinks = random.randint(0, maxLinks)
-    links = random.sample(range(0, numPages-1), numLinks) 
+    links = random.sample(range(0, numPages - 1), numLinks)
     for j in links:
-        #page id
+        # page id
         pages.write(str(i))
         pages.write(" ")
-        #link id
+        # link id
         pages.write(str(j))
         pages.write("\n")
-        print("written "+str(numLinks) +" links for page:"+ str(i))
-
+        print("written " + str(numLinks) + " links for page:" + str(i))
 
 pages.close()

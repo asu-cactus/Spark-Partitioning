4918845e46d70aed3a7328f32fee9ca45144a174
PratikBarhate <pratikbarhate00@outlook.com>
2020-04-13 20:44:51 -0700

    Correction in the Q9

--- src/main/scala/edu/asu/tpch/queries/Q9.scala
+++ src/main/scala/edu/asu/tpch/queries/Q9.scala
@@ -30,7 +30,6 @@ private[tpch] object Q9 extends QueryOps {
     val natSup =
       nationDf.join(supplierDf, col(N_NATIONKEY) === supplierDf(S_NATIONKEY))
 
-    // TODO: Check the query for correctness
     linePart
       .join(natSup, col(L_SUPPKEY) === natSup(S_SUPPKEY))
       .join(
@@ -42,7 +41,10 @@ private[tpch] object Q9 extends QueryOps {
       .select(
         col(N_NAME),
         getYear(col(O_ORDERDATE)).as("o_year"),
-        col(L_QUANTITY).as("amount")
+        (
+          col(L_EXTENDEDPRICE) * (lit(1) - col(L_DISCOUNT)) -
+            col(PS_SUPPLYCOST) * col(L_QUANTITY)
+        ).as("amount")
       )
       .groupBy(N_NAME, "o_year")
       .agg(sum(col("amount")))

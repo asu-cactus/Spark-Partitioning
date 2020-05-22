1f441484864d0fb2e2a2f6dc172187bd75d1fe93
Amitabh Das <amitab.das@outlook.com>
2020-05-22 07:21:09 -0700

    Move merge code inside graph

--- src/main/java/edu/asu/overheadanalysis/supergraph/SuperGraph.java
+++ src/main/java/edu/asu/overheadanalysis/supergraph/SuperGraph.java
@@ -73,6 +73,37 @@ class Graph {
         nodes = new ArrayList<Node>(size);
     }
 
+    public static Graph mergeTwoGraphs(Graph a, Graph b) {
+        int aCount = 0, bCount = 0;
+
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (int i = 0; aCount < a.size || bCount < b.size; i++) {
+            Node node = new Node(i, -1);
+            if (aCount < a.size && bCount < b.size && a.nodes.get(aCount).getColor().equals(b.nodes.get(bCount).getColor())) {
+                node.setColor(a.nodes.get(aCount).getColor());
+                node.merge(a.nodes.get(aCount).adj);
+                node.merge(b.nodes.get(bCount).adj);
+                nodes.add(node);
+                aCount++;
+                bCount++;
+            } else if (aCount >= a.size) {
+                node.setColor(b.nodes.get(bCount).getColor());
+                node.merge(b.nodes.get(bCount).adj);
+                nodes.add(node);
+                bCount++;
+            } else {
+                node.setColor(a.nodes.get(aCount).getColor());
+                node.merge(a.nodes.get(aCount).adj);
+                nodes.add(node);
+                aCount++;
+            }
+        }
+
+        Graph newGraph = new Graph(nodes.size());
+        newGraph.nodes = nodes;
+        return newGraph;
+    }
+
     public void newNode(Color color, int pos, int size) {
         Node node = new Node(pos, size);
         node.setColor(color);
@@ -113,37 +144,6 @@ class Graph {
 }
 
 public class SuperGraph {
-    public static Graph mergeTwoGraphs(Graph a, Graph b) {
-        int aCount = 0, bCount = 0;
-
-        ArrayList<Node> nodes = new ArrayList<>();
-        for (int i = 0; aCount < a.size || bCount < b.size; i++) {
-            Node node = new Node(i, -1);
-            if (aCount < a.size && bCount < b.size && a.nodes.get(aCount).getColor().equals(b.nodes.get(bCount).getColor())) {
-                node.setColor(a.nodes.get(aCount).getColor());
-                node.merge(a.nodes.get(aCount).adj);
-                node.merge(b.nodes.get(bCount).adj);
-                nodes.add(node);
-                aCount++;
-                bCount++;
-            } else if (aCount >= a.size) {
-                node.setColor(b.nodes.get(bCount).getColor());
-                node.merge(b.nodes.get(bCount).adj);
-                nodes.add(node);
-                bCount++;
-            } else {
-                node.setColor(a.nodes.get(aCount).getColor());
-                node.merge(a.nodes.get(aCount).adj);
-                nodes.add(node);
-                aCount++;
-            }
-        }
-
-        Graph newGraph = new Graph(nodes.size());
-        newGraph.nodes = nodes;
-        return newGraph;
-    }
-
     public static void main(String[] args) {
         int graph_count = 2;
         Random random = new Random();
@@ -166,7 +166,7 @@ public class SuperGraph {
         System.out.println("--------------------------------------");
         graphs.get(1).print();
         System.out.println("--------------------------------------");
-        Graph newGraph = mergeTwoGraphs(graphs.get(0), graphs.get(1));
+        Graph newGraph = Graph.mergeTwoGraphs(graphs.get(0), graphs.get(1));
         newGraph.print();
     }
 }

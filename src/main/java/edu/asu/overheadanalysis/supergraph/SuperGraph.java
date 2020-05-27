a14cf863aea670175cc65a388966a1c2889378fe
Amitabh Das <amitab.das@outlook.com>
2020-05-27 16:59:21 -0700

    batch run

--- src/main/java/edu/asu/overheadanalysis/supergraph/SuperGraph.java
+++ src/main/java/edu/asu/overheadanalysis/supergraph/SuperGraph.java
@@ -1,5 +1,6 @@
 package edu.asu.overheadanalysis.supergraph;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -23,10 +24,10 @@ public class SuperGraph {
         Graph a = graphs.get(i);
         Graph b = graphs.get(i + 1);
         futures.add(
-          executorService.submit(
-            () -> {
-              return Graph.mergeTwoGraphs(a, b);
-            }));
+            executorService.submit(
+                () -> {
+                  return Graph.mergeTwoGraphs(a, b);
+                }));
       }
 
       for (Future<Graph> future : futures) {
@@ -37,18 +38,13 @@ public class SuperGraph {
       tempList = new ArrayList<>();
       futures.clear();
     }
-
-    executorService.shutdown();
     return graphs.get(0);
   }
 
-  public static void main(String[] args) throws ExecutionException, InterruptedException {
-    int graph_count = Integer.parseInt(args[0]);
+  public static int genRandDAG(int count, ArrayList<Graph> graphs) {
     Random random = new Random();
-
-    List<Graph> graphs = new ArrayList<>();
     int totalNodes = 0;
-    for (int i = 0; i < graph_count; i++) {
+    for (int i = 0; i < count; i++) {
       int size = random.nextInt(9) + 2;
       Graph graph = new Graph();
       graph.setRandom(size);
@@ -57,24 +53,57 @@ public class SuperGraph {
       totalNodes += size;
     }
 
-    int avgNodes = totalNodes / graph_count;
+    return totalNodes;
+  }
 
-    //        graphs.get(0).print();
-    //        System.out.println("--------------------------------------");
-    //        graphs.get(1).print();
-    //        System.out.println("--------------------------------------");
-    long startTime = System.currentTimeMillis();
-    Graph newGraph = kWayMerge(graphs);
-    long endTime = System.currentTimeMillis();
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    int graph_count = Integer.parseInt(args[0]);
+    int batchSize = Integer.parseInt(args[1]);
+
+    ArrayList<Graph> supergraphs = new ArrayList<>();
+    ArrayList<Graph> graphs = new ArrayList<>();
+    long totalNodes = 0;
+    int totalTime = 0;
+    int graphsLeft = graph_count;
+
+    while (graphsLeft > 0) {
+      int genCount = Math.min(graphsLeft, batchSize);
+      System.out.println(
+          new SimpleDateFormat("HH:mm:ss").format(new Date()) + " Creating batch size " + genCount);
+      totalNodes += genRandDAG(genCount, graphs);
+      System.out.println(new SimpleDateFormat("HH:mm:ss").format(new Date()) + " Batch created");
+      graphsLeft -= genCount;
+
+      System.out.println(new SimpleDateFormat("HH:mm:ss").format(new Date()) + " Starting merge");
+      long startTime = System.currentTimeMillis();
+      supergraphs.add(kWayMerge(graphs));
+      long endTime = System.currentTimeMillis();
+      totalTime += endTime - startTime;
+      System.out.println(new SimpleDateFormat("HH:mm:ss").format(new Date()) + " Merge completed");
+      graphs.clear();
+    }
+
+    if (supergraphs.size() > 1) {
+      long startTime = System.currentTimeMillis();
+      Graph finalGraph = kWayMerge(supergraphs);
+      long endTime = System.currentTimeMillis();
+      totalTime += endTime - startTime;
+      finalGraph.print();
+    } else {
+      supergraphs.get(0).print();
+    }
+
+    long avgNodes = totalNodes / graph_count;
+    executorService.shutdown();
 
     System.out.println(
-        "Merging of "
+        (new SimpleDateFormat("HH:mm:ss").format(new Date()))
+            + " Merging of "
             + graph_count
             + " DAGS with avg nodes: "
             + avgNodes
             + " took "
-            + (endTime - startTime)
+            + totalTime
             + " milliseconds");
-    newGraph.print();
   }
 }
